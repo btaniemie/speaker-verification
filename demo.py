de21e2995275d306d17bacf753e21fad2d7770ca
Minh Le <81462198+btaniemie@users.noreply.github.com>
2024-10-19 12:40:01 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,7 @@
-# streamlit_audio_recorder by stefanrmmr (rs. analytics) - version January 2023
-
 import streamlit as st
 from st_audiorec import st_audiorec
 
-# DESIGN implement changes to the standard streamlit UI/UX
-# --> optional, not relevant for the functionality of the component!
+
 st.set_page_config(page_title="streamlit_audio_recorder")
 # Design move app further up and remove top padding
 st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''',
@@ -21,32 +18,14 @@ st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
 
 def audiorec_demo_app():
 
-    # TITLE and Creator information
-    st.title('streamlit audio recorder')
-    st.markdown('Implemented by '
-        '[Stefan Rummer](https://www.linkedin.com/in/stefanrmmr/) - '
-        'view project source code on '
-                
-        '[GitHub](https://github.com/stefanrmmr/streamlit-audio-recorder)')
+    st.title('speaker verification')
+    st.markdown('')
     st.write('\n\n')
 
-    # TUTORIAL: How to use STREAMLIT AUDIO RECORDER?
-    # by calling this function an instance of the audio recorder is created
-    # once a recording is completed, audio data will be saved to wav_audio_data
-
-    wav_audio_data = st_audiorec() # tadaaaa! yes, that's it! :D
-
-    # add some spacing and informative messages
-    col_info, col_space = st.columns([0.57, 0.43])
-    with col_info:
-        st.write('\n')  # add vertical spacer
-        st.write('\n')  # add vertical spacer
+    wav_audio_data = st_audiorec() 
 
     if wav_audio_data is not None:
-        # display audio data as received on the Python side
-        col_playback, col_space = st.columns([0.58,0.42])
-        with col_playback:
-            st.audio(wav_audio_data, format='audio/wav')
+        st.audio(wav_audio_data, format='audio/wav')
 
 
 if __name__ == '__main__':
